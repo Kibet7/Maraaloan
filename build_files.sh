3c7b461ed7c211a7f021f56241e333e1bcb5be44
Frankibet <frankabule101@gmail.com>
2025-02-26 09:21:28 +0300

    Fix staticfiles_build directory issue

--- build_files.sh
+++ build_files.sh
@@ -7,5 +7,7 @@ python3 -m pip install -r requirements.txt
 # Apply migrations
 python3 manage.py migrate --noinput
 
-# Collect static files
-python3 manage.py collectstatic --noinput
+# Collect static files (fixing missing directory issue)
+mkdir -p staticfiles_build  # Ensure the directory exists
+python3 manage.py collectstatic --noinput --clear --verbosity 3 -i admin -i rest_framework
+mv static staticfiles_build  # Move static files to expected directory
