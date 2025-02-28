67626bdf459ad2d62390df1df2cfce1a2ae5488c
Frankibet <frankabule101@gmail.com>
2025-02-28 17:13:26 +0300

    initial commit

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,2 @@
-#!/bin/bash
-
-# Upgrade pip and install dependencies
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
-
-# Apply migrations
-python3 manage.py migrate --noinput
-
-# Collect static files (fixing missing directory issue)
-mkdir -p staticfiles_build  # Ensure the directory exists
-python3 manage.py collectstatic --noinput --clear --verbosity 3 -i admin -i rest_framework
-mv static staticfiles_build  # Move static files to expected directory
+pip install -r requirements.txt
+python manage.py collectstatic
